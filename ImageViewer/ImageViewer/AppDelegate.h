6ec1cbca927bc1804a813a90dca2f0a30663003e
lyleKP <v2top1@163.com>
2017-02-15 16:01:09 +0800

    添加样例工程和资源

---
+++ ImageViewer/ImageViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageViewer
+//
+//  Created by lyleKP on 2017/2/15.
+//  Copyright © 2017年 lyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageViewer/ImageViewer/RWUIControls.framework/Versions/A/Headers/RWUIControls.h
@@ -0,0 +1,11 @@
+//
+//  RWUIControls.h
+//  RWUIControls
+//
+//  Created by lyleKP on 2017/2/15.
+//  Copyright © 2017年 lyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+// Knob Control
+#import <RWUIControls/RWKnobControl.h>
